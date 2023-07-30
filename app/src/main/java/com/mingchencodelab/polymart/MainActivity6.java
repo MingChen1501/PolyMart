2fbc0dcae21a3f24a996bcc96bb6d422f8afa3b4
MinhTran <tminh400@gmail.com>
2023-07-30 11:47:25 +0700

    update lab6

--- app/src/main/java/com/mingchencodelab/polymart/MainActivity6.java
+++ app/src/main/java/com/mingchencodelab/polymart/MainActivity6.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -11,43 +12,45 @@ import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.gson.JsonObject;
 
 import org.json.JSONObject;
 
 public class MainActivity6 extends AppCompatActivity {
     RequestQueue requestQueue;
-    String host = "http://localhost:3000";
+    String host = "http://172.188.32.218/";
     String url = host + "/posts";
-    Button btnGet;
-    Button btnGet2;
+    EditText edtId;
+    Button btnTimBaiVietVoiId;
+    Button btnDanhSachBaiViet;
     TextView txtResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main6);
-        btnGet = findViewById(R.id.lab6_btn_get);
-        btnGet2 = findViewById(R.id.lab6_btn_get_array);
+        btnTimBaiVietVoiId = findViewById(R.id.lab6_btn_get);
+        btnDanhSachBaiViet = findViewById(R.id.lab6_btn_get_array);
+        edtId = findViewById(R.id.lab6_et_id);
         txtResult = findViewById(R.id.lab6_txt_res);
         requestQueue = Volley.newRequestQueue(this);
-        btnGet.setOnClickListener(v -> {
+        btnTimBaiVietVoiId.setOnClickListener(v -> {
             getStringRequest();
         });
-        btnGet2.setOnClickListener(v -> {
+        btnDanhSachBaiViet.setOnClickListener(v -> {
             getArrayRequest();
         });
 
     }
 
     private void getArrayRequest() {
+        txtResult.setText("");
         JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET,
                 url,
                 null,
                 response -> {
             for (int i = 0; i < response.length(); i++) {
                 JSONObject jsonObject = response.optJSONObject(i);
+                txtResult.append(jsonObject.optString("id") + "\n");
                 txtResult.append(jsonObject.optString("title") + "\n");
-                txtResult.append(jsonObject.optString("content") + "\n");
             }
         }, error -> {
             txtResult.setText(error.getMessage());
@@ -56,7 +59,9 @@ public class MainActivity6 extends AppCompatActivity {
     }
 
     private void getStringRequest() {
-        StringRequest request = new StringRequest(Request.Method.GET, url + "/1", response -> {
+        String id = edtId.getText().toString();
+        txtResult.setText("");
+        StringRequest request = new StringRequest(Request.Method.GET, url + "/" + id, response -> {
             txtResult.setText(response);
         }, error -> {
             txtResult.setText(error.getMessage());
